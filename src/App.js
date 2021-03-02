601c1436749ab767dbd9f5b4cdfa2359dc1b629e
Radzio1976 <r.tomaszewski@gmail.com>
2021-03-02 11:34:54 +0100

    Added Home and Cart components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import react from 'react';
-import {BrowserRouter, Switch, Router} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Cart from './components/Cart';
 
 class App extends react.Component {
   render() {
     return(
       <div id="App">
         <BrowserRouter>
+        <Nav />
           <Switch>
             <Route path='/' exact component={Home} />
             <Route path='/cart' component={Cart} />
