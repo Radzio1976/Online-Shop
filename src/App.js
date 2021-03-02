e4ea601310aee3a4504ee72860039ea9c9fcbe85
Radzio1976 <r.tomaszewski@gmail.com>
2021-03-02 15:11:03 +0100

    Added functionality to navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import react from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import axios from 'axios';
 import Nav from './components/Nav';
 import Home from './components/Home';
 import Cart from './components/Cart';
 
 class App extends react.Component {
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    axios.get('https://simko.it/themes/simko-it/assets/products.json')
+    .then(res => {
+      this.setState({
+        products: res.data
+      })
+    })
+    .catch(err => {
+      console.log("Nie udało się pobrać bazy produktów", err)
+    })
+  }
   render() {
     return(
       <div id="App">
         <BrowserRouter>
-        <Nav />
+        <Nav data={this.state.products} />
           <Switch>
             <Route path='/' exact component={Home} />
             <Route path='/cart' component={Cart} />
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,66 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 class Nav extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
     render() {
+        const products = this.props.data;
+
+        function saleProducts() {
+            let saleProd = 0;
+            for (let i=0; i<products.length; i++) {
+                if (products[i].marks !== null) {
+                    for (let k=0; k<products[i].marks.length; k++) {
+                        if (products[i].marks[k] === "Sale") {
+                            saleProd += 1;
+                        }
+                    }
+                }
+            }
+            return saleProd;
+        }
+
+        function samsungProducts() {
+            let samsungProd = 0;
+            for (let i=0; i<products.length; i++) {
+                if (products[i].producer === "Samsung") {
+                    samsungProd += 1;
+                }
+            }
+            return samsungProd;
+        }
+
+        function appleProducts() {
+            let appleProd = 0;
+            for (let i=0; i<products.length; i++) {
+                if (products[i].producer === "Apple") {
+                    appleProd += 1;
+                }
+            }
+            return appleProd;
+        }
+
         return(
-            <h1>Nawigacja</h1>
+            <div className="navigation">
+                <nav className="nav">
+                    <ul>
+                        <li onClick={() => this.props.history.push('/')}>Homepage</li>
+                        <li>On Sale <span>{saleProducts()}</span></li>
+                        <li>Samsung <span>{samsungProducts()}</span></li>
+                        <li>Apple <span>{appleProducts()}</span></li>
+                    </ul>
+                </nav>
+                <nav className="nav">
+                    <ul>
+                        <li>Cart <span>{100} PLN</span></li>
+                    </ul>
+                </nav>
+            </div>
         )
     }
 }
 
-export default Nav;
+export default withRouter(Nav);
