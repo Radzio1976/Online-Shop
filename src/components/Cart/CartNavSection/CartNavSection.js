6998584ac3972c000869c135f4a2f2834228b90d
Radzio1976 <r.tomaszewski@gmail.com>
2021-03-17 10:25:54 +0100

    Added CartTableSection

---
+++ src/components/Cart/CartNavSection/CartNavSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CartNavSection extends React.Component {
+    render() {       
+        return(
+            <div className="cart-nav">
+                <h1>Cart Checkout</h1>
+                <button>Submit Order</button>
+            </div>
+        )
+    }
+}
+
+export default CartNavSection;
