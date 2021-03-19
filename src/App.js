eac330be13f6a05f0568d75e674ee3167344ff61
Radzio1976 <r.tomaszewski@gmail.com>
2021-03-19 14:16:47 +0100

    Fixed ternary operators in changeLimit function

--- src/App.js
+++ src/App.js
@@ -57,27 +57,11 @@ class App extends react.Component {
   }
 
   changeLimit = (value) => {
-      this.setState({
-          paginationCounter: 1,
-          firstProduct: 0,
-          lastProduct: 15
-      })
-
-  if (value === "32") {
-      this.setState({
-          paginationCounter: 1,
-          firstProduct: 0,
-          lastProduct: 31
-      })
-  }
-  if (value === "64") {
-      this.setState({
-          paginationCounter: 1,
-          firstProduct: 0,
-          lastProduct: 63
-      })
-  }
-
+    this.setState({
+        paginationCounter: 1,
+        firstProduct: 0,
+        lastProduct: value === "16" ? 15 : value === "32" ? 31 : value === "64" ? 63 : this.state.lastProduct
+    })
   }
 
   handleChange = (key, value) => {
